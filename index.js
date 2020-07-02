7f25e9840f2adf56104171ab36501b10e67cfe79
Jonah <jonahx@gmail.com>
2020-07-02 14:23:41 -0400

    Try fixing async

--- index.js
+++ index.js
@@ -2,23 +2,20 @@ const core = require('@actions/core')
 // const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // const time = (new Date()).toTimeString();
-  const exitCode = await exec.exec('./is_rebased')
-  if (exitCode != 0) {
+async () => {
+  try {
+    const exitCode = await exec.exec('./is_rebased')
+
+    if (exitCode != 0) {
+      core.setFailed(
+        "Pull requests must be rebased on master, and cannot contain any " +
+        "merge commits."
+      )
+    }
+  } catch (error) {
     core.setFailed(
-      "Pull requests must be rebased on master, and cannot contain any merge " +
-      "commits."
+      "The GH action itself failed unexpectedly: " + error.message
     )
   }
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed("The GH action itself failed unexpectedly: " + error.message);
-}
+}()
 
