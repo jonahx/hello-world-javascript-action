dc0ed67d1403fd4a456caadd58e26d2aa61ccd51
Jonah <jonahx@gmail.com>
2020-07-05 18:17:45 -0400

    Fix error display

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const core = require('@actions/core')
 // const github = require('@actions/github')
 const exec = require('@actions/exec');  // You need the semicolon!
 
+
+const failureMsg = "Pull requests must be rebased on master, and cannot " +
+  "contain any merge commits."
+
 (async () => {
   try {
     const exitCode = await exec.exec('./is_rebased')
 
     if (exitCode != 0) {
-      core.setFailed(
-        "Pull requests must be rebased on master, and cannot contain any " +
-        "merge commits."
-      )
+      core.setFailed(failureMsg)
     }
   } catch (error) {
-    core.setFailed(
-      "The GH action itself failed unexpectedly: " + error.message
-    )
+    core.setFailed(failureMsg)
   }
 })()
 
